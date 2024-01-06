d627d45ecee65fbe82fc844fa0f38d016829c5e9
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-01-06 19:29:14 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Vinura Pathirana <154809219+vinurapathirana@users.noreply.github.com>

---
+++ database.php
@@ -0,0 +1,26 @@
+<?php
+
+class Database
+{
+    private $host = "localhost";
+    private $username = "root";
+    private $password = "";
+    private $database = "test_pck";
+    public $conn;
+
+    public function __construct()
+    {
+        try {
+            $this->conn = new PDO("mysql:host=$this->host;dbname=$this->database", $this->username, $this->password);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            echo "Connection failed: " . $e->getMessage();
+        }
+    }
+
+    public function closeConnection()
+    {
+        $this->conn = null;
+    }
+}
+?>

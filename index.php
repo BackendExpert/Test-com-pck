8cbbd90f47672d75f21ec1f4b88a334c67a59030
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-01-06 21:45:48 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Vinura Pathirana <154809219+vinurapathirana@users.noreply.github.com>

--- index.php
+++ index.php
@@ -1,17 +1,4 @@
 <?php 
-    require_once 'database.php';
-    require_once 'user.php';
 
-    $dbc = new Database();
-    $user = new User($dbc);
-
-    // $user->createUser('Jehan', 'jehan@example.com', password_hash('2563985', PASSWORD_DEFAULT));
-
-    $users = $user->getUsers();
-    print_r($users);
-
-    // $user->updateUser(2, 'JehanKandy', 'jkany@example.com', password_hash('5563333', PASSWORD_DEFAULT));
-
-    $user->deleteUser(2);
 
 ?>

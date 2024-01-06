6e6694083e91f9dda15a4aac2c200c021b1e983a
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-01-06 21:50:18 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Vinura Pathirana <154809219+vinurapathirana@users.noreply.github.com>

--- index.php
+++ index.php
@@ -1,4 +1,36 @@
-<?php 
+<?php
+
+include 'user.php';
+
+// Usage example:
+$crud = new Crud($dbConfig);
 
 
-?>
+$username = 'john_doe';
+$email = 'john.doe@example.com';
+$crud->create($username, $email);
+
+//-------------------------
+$users = $crud->read();
+echo "Read all users:\n";
+print_r($users);
+
+// //-------------------------------
+// $userToUpdate = 1; 
+// $newUsername = 'updated_john_doe';
+// $newEmail = 'updated.john.doe@example.com';
+// $crud->update($userToUpdate, $newUsername, $newEmail);
+
+// $updatedUsers = $crud->read();
+// echo "Read all users after update:\n";
+// print_r($updatedUsers);
+
+// //--------------------------
+
+
+// $userToDelete = 2; 
+// $crud->delete($userToDelete);
+
+// $remainingUsers = $crud->read();
+// echo "Read all users after delete:\n";
+// print_r($remainingUsers);
--- user.php
+++ user.php
@@ -1,6 +1,6 @@
 <?php
 
-include 'config.php';
+include 'conn_db.php';
 
 class Crud
 {

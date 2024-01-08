85f9c046377b858458b28decfabf4d3c04c1c4f6
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-01-08 08:57:58 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Vinura Pathirana <154809219+VinuraPathirana@users.noreply.github.com>

--- config/app.php
+++ config/app.php
@@ -4,3 +4,8 @@ define ('HOST_DB','localhost');
 define ('HOST_USER','root');
 define ('HOST_PASS','');
 define ('HOST_DBNAME','test_pck');
+
+include_once('db_con.php');
+$dbc = new DatabaseConn;
+
+?>
